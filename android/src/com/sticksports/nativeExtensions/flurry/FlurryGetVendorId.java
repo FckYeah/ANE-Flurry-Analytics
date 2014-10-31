dc1c1aacb38d31c336fb3c7a7b50eddac9aa6de9
Karl DeBisschop <karl.debisschop@pearson.com>
2014-10-31 18:30:14 -0400

    no getVendorId in java

--- android/src/com/sticksports/nativeExtensions/flurry/FlurryGetVendorId.java
+++ android/src/com/sticksports/nativeExtensions/flurry/FlurryGetVendorId.java
@@ -1,7 +1,5 @@
 package com.sticksports.nativeExtensions.flurry;
 
-import android.util.Log;
-
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
@@ -13,15 +11,6 @@ public class FlurryGetVendorId implements FREFunction
 	@Override
 	public FREObject call( FREContext context, FREObject[] args )
 	{
-		try
-		{
-			String versionString = FlurryAgent.getVendorId();
-			return FREObject.newObject( versionString );
-		}
-		catch ( Exception exception )
-		{
-			Log.w( "Flurry", exception );
-		}
 		return null;
 	}
 }
